95b04c069a8553e4a515b1b73332fc7e15f2b46e
bader <bader5uk@hotmail.com>
2024-09-22 10:04:03 +0400

    first iteration with example sentence

---
+++ longestWord.java
@@ -0,0 +1,16 @@
+public class longestWord {
+    //assign a string with a sentence
+    //write  a programme that can identify the longest word based on character count
+    //print a statement that shows the longest word
+    public static void main(String[] args) {
+        String exampleSentence = "The quick brown fox jumped over the lazy dog";
+        String[] words = exampleSentence.split(" ");
+        String longestWord = "";
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        System.out.println("Longest word: " + longestWord);
+    }
+}
